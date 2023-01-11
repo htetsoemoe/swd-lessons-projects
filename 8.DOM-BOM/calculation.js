6f43198809519ce5b8124fae3d77d0f79a0273f5
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-01-11 11:52:35 +0630

    Day50 Intro JS DOM

---
+++ 8.DOM-BOM/calculation.js
@@ -0,0 +1,31 @@
+//String in JavaScript
+
+/**
+ * 
+// + operator
+console.log("2" + "2");
+console.log("2" + 2);
+console.log(2 + "2");
+console.log(2 + 2);
+
+// - operator work normal calculation
+console.log("2" - "2");
+console.log("2" - 2);
+console.log(2 - "2");
+console.log(2 - 2);
+
+// * operator work normal calculation
+console.log("2" * "2");
+console.log("2" * 2);
+console.log(2 * "2");
+console.log(2 * 2);
+
+// / operator work normal calculation
+console.log("2" / "2");
+console.log("2" / 2);
+console.log(2 / "2");
+console.log(2 / 2);
+ */
+
+
+
--- 8.DOM-BOM/dom-test.js
+++ 8.DOM-BOM/dom-test.js
@@ -1,9 +1,50 @@
 // input
-const area = (w, b) => {
-    return parseFloat(w) * parseFloat(b) + " sqft";
-}
+// const area = (w, b) => {
+//     return parseFloat(w) * parseFloat(b) + " sqft";
+// }
 
-const width = window.prompt("Enter Width");
-const breadth = window.prompt("Enter Breadth");
+// const width = window.prompt("Enter Width");
+// const breadth = window.prompt("Enter Breadth");
 
-window.alert(area(width, breadth));
+// window.alert(area(width, breadth));
+
+const elementById = document.getElementById("");
+
+// getElementsByTagName returns HTMLCollections
+const elementByTagName = document.getElementsByTagName("");
+
+// querySelector select only first dom element in page, but it can select with tagName, ID and Class
+const querySelector = document.querySelector("");
+
+
+// querySelectorAll returns (NodeList()) all element in page
+const querySelectorAll = document.querySelectorAll("h2");
+
+/**
+ * Lesson of => document.querySelector("h2").classList  =>  returns DOMTokenList(2), you can treat like array object
+ * 
+ *           => DOMTokenList
+ *                  document.querySelector("h2").classList.add()
+ *                  document.querySelector("h2").classList.remove()
+ *                  document.querySelector("h2").classList.contains()
+ *                  document.querySelector("h2").classList.toggle()
+ *                  document.querySelector("h2").classList.replace(oldToken, newToke)
+ *                  document.querySelector("h2").classList.forEach(()=> {})
+ */
+
+// Get actual file from file input
+// single file
+/**
+ * document.querySelector("#propic").files; => returns FileList
+    document.querySelector("#propic").files[0];
+
+
+    // mulitple files
+    document.querySelector("#skill").files; => returns FileList
+    document.querySelector("#skill").files[2];
+ */
+
+
+
+
+
