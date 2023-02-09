99e778864218d463064f0397ff630bb7484649e8
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-02-09 17:20:08 +0630

    finished card detail carousel

--- 9.JS Mini Projects/shopping-cart/app.js
+++ 9.JS Mini Projects/shopping-cart/app.js
@@ -2,11 +2,15 @@
 
 // variables
 const productCards = document.querySelector("#productCards");
+const app = document.querySelector("#app");
+const productDetailModal = new bootstrap.Modal("#productDetailModal");
+
+// productDetailModal.show();
 
 // create rating function for product
 const star = no => {
     let starString = "";
-    for(let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 5; i++) {
         if (Math.ceil(no) <= i) { // if no=3 <= 1 and 2 and 3
             starString += "<i class='bi bi-star'></i>"
         } else {
@@ -38,7 +42,7 @@ const createProductCard = (product) => {
 
                 <div class="d-flex justify-content-between align-items-center mb-2">
                     <div class="badge text-dark text-capitalize bg-light">
-                        ${product.category.replaceAll("-"," ")}
+                        ${product.category.replaceAll("-", " ")}
                     </div>
                     <div class="">
                         ${star(product.rating)}
@@ -61,5 +65,90 @@ const createProductCard = (product) => {
 
 // append a column with product cards to a 'ProductCards' row
 products.forEach(product => {
-    productCards.append(createProductCard(product)); 
+    productCards.append(createProductCard(product));
+});
+
+// create product detail carousel in model-body
+const productDetailCarousel = arr => {
+    let slides = "";
+    let indicators = "";
+
+    arr.forEach((el, index) => {
+        slides += `<div class="carousel-item ${index === 0 && "active"}">
+            <img src="${el}"
+            class="d-block w-100 product-detail-img"
+            alt="..."
+            />
+        </div>`;
+
+        indicators += `<button type="button"
+            data-bs-target="#productDetailCarousel"
+            data-bs-slide-to="${index}"
+            class="${index === 0 && "active"}"
+            aria-current="true"
+            aria-label="Slide 1"
+        ></button>`;
+    });
+
+    return { slides, indicators };
+}
+
+// create 'Carousel' in 'Modal'
+app.addEventListener('click', event => {
+    //console.log(event.target.closest(".product-card"));
+    if (event.target.closest(".product-card")) {
+        const currentCard = event.target.closest(".product-card");
+        const currentProductId = currentCard.getAttribute("product-id");
+        const currentProduct = products.find(product => product.id == currentProductId);
+
+        console.log(currentProduct);
+
+        productDetailModal._element.querySelector(".modal-title").innerText = currentProduct.title;
+
+        productDetailModal._element.querySelector(".modal-body").innerHTML = `
+            <div id="productDetailCarousel" class="carousel slide">
+                <div class="carousel-indicators">
+                    ${productDetailCarousel(currentProduct.images).indicators}
+                </div>
+                <div class="carousel-inner">
+                    ${productDetailCarousel(currentProduct.images).slides}
+                </div>
+
+                <button class="carousel-control-prev" type="button"
+                data-bs-target="#productDetailCarousel"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+                </button>
+
+                <button class="carousel-control-next" type="button"
+                data-bs-target="#productDetailCarousel"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            <div class="d-flex justify-content-between align-items-center mt-2 mb-2">
+                <div class="badge text-dark text-capitalize bg-light">
+                    ${currentProduct.category.replaceAll("-", " ")}
+                </div>
+                <div class="">
+                    ${star(currentProduct.rating)}
+                </div>
+            </div>
+
+            <div class="px-2">
+                <p class="mb-2">${currentProduct.description}</p>
+                <p class="">$ ${currentProduct.price}</p>
+            </div>
+
+        `;
+
+        productDetailModal.show();
+    }
 });
