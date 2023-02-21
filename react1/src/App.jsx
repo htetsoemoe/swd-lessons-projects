57bcdeb66052594d7ee56cc18d1501b9c166a6ec
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-02-21 10:28:11 +0630

    day1 part2

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
+  const array = ["Super Man", "Bat Man", "Iron Man"];
+  const sayHello = () => console.log("Hello!!");
+
   return (
     <div>
       <h1>Hello React with Vite</h1>
+
+      <button onClick={sayHello}>Say Hello</button>
+
+      <h1>{true ? "Yes! True..." : "Ahh! False..."}</h1>
+      <h1>{true && "Hey There!"}</h1>
+
+      <div>
+        {array.map(item => (
+          <div className="card">
+            <img src="/public/product.jpg" style={{width: "250px"}} alt="" />
+            <h1 className="heading">Custom Bike</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, magnam!</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
