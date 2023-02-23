d78391b8eb6a55dc901378ccb8fe357f42a76ee8
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-02-23 11:15:36 +0630

    updated card.jsx

--- 14.React/shopping-cart/components/Card.jsx
+++ 14.React/shopping-cart/components/Card.jsx
@@ -2,23 +2,21 @@ import React from 'react'
 
 const Card = ({product}) => {
     return (
-        <div>
-            <div className='card px-3 py-3 bg-light' key={product.id} style={{ width: "18rem" }}>
-                <div className="card-body">
+        <div className='card px-3 py-3 bg-light' key={product.id} style={{ width: "18rem" }}>
+            <div className="card-body">
 
-                    <img src={product.image} height="100px" alt="" />
+                <img src={product.image} height="100px" alt="" />
 
-                    <h4 className="card-title text-truncate">
-                        {product.title}
-                    </h4>
+                <h4 className="card-title text-truncate">
+                    {product.title}
+                </h4>
 
-                    <p className='card-text text-truncate text-sm-start'>
-                        {product.description}
-                    </p>
-                </div>
-
-                <button className="btn btn-primary">Add To Cart</button>
+                <p className='card-text text-truncate text-sm-start'>
+                    {product.description}
+                </p>
             </div>
+
+            <button className="btn btn-primary">Add To Cart</button>
         </div>
     )
 }
