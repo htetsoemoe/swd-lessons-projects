27eaef1093fe2dcd12df2a2e39f816e74088bec2
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-05-10 12:10:38 +0630

    updated register login component with mantine form

--- 14.React/rtk-query-auth/src/pages/Login.jsx
+++ 14.React/rtk-query-auth/src/pages/Login.jsx
@@ -1,55 +1,100 @@
 import React, { useState } from 'react'
-import { TextInput } from '@mantine/core';
+import { Loader, TextInput } from '@mantine/core';
 import { PasswordInput } from '@mantine/core';
 import { Link, useNavigate } from 'react-router-dom'
 import { useLoginMutation } from '../redux/api/authApi';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../redux/services/authSlice';
+import { useForm } from '@mantine/form';
 
 const Login = () => {
-  const [email, setEmail] = useState("kohtetmdy@gmail.com")
-  const [password, setPassword] = useState("123456789")
-
+  
   const navigate = useNavigate()
-  const [login] = useLoginMutation()
+  const [login, {isLoading}] = useLoginMutation()
 
   // for add login user to global state using rtk
   const dispatch = useDispatch()
 
-  const loginHandler = async (event) => {
-    try {
-      event.preventDefault()
-      const user = { email, password }
-      const {data} = await login(user);
-      dispatch(addUser({user: data?.user, token: data?.token}))
-      console.log(data)
-      if (data?.success) {
-        navigate("/")
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  // Using Mantine use-form
+  const form = useForm({
+    initialValues: {
+      email: 'kohtetmdy@gmail.com',
+      password: '123456789',
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      password: (value) => value.length < 8 ? "Password must have at least 8 letters" : null,
+    },
+  });
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <form onSubmit={loginHandler} className='flex flex-col w-96 gap-10 shadow-lg p-7'>
+      <form onSubmit={form.onSubmit(async (values) => {
+        try {
+          const { data } = await login(values)
+          console.log(data) // this is promise object
+          console.log(values)
+          // set successfully user object and token in browser cookie
+          dispatch(addUser({ user: data?.user, token: data?.token }))
+
+          // If login success, navigate to dashboard
+          if (data?.success === true) {
+            navigate('/')
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      })}
+        className='flex flex-col w-96 gap-10 shadow-lg p-7'>
         <h2 className='text-zinc-900 text-2xl font-semibold'>Log In</h2>
 
-        <TextInput value={email} onChange={e => setEmail(e.target.value)} placeholder='Enter your email' />
-        <PasswordInput value={password} onChange={e => setPassword(e.target.value)} placeholder='Enter your password' />
+        <TextInput
+          name='email'
+          {...form.getInputProps('email')}
+          placeholder='Enter your email' />
+
+        <PasswordInput
+          name='password'
+          {...form.getInputProps('password')}
+          placeholder='Enter your password' />
 
         <div className="flex gap-7">
           <p className='select-none text-zinc-800'>Already have an account?</p>
-          <Link to={'/login'}>
-            <p className='cursor-pointer underline select-none text-zinc-800'>Login</p>
+          <Link to={'/register'}>
+            <p className='cursor-pointer underline select-none text-zinc-800'>Register</p>
           </Link>
         </div>
 
-        <button type="submit" className='px-5 py-2 bg-zinc-900 text-white rounded'>Log In</button>
+        <button disabled={isLoading && true}
+          type="submit" 
+          className='px-5 py-2 bg-zinc-900 text-white rounded'>
+          {isLoading ? (<Loader className='mx-auto block'/>): "Log In"}    
+        </button>
       </form>
     </div>
   )
 }
 
 export default Login
+
+/**
+   * Before using mantine use-form
+   * 
+   * const [email, setEmail] = useState("kohtetmdy@gmail.com")
+   * const [password, setPassword] = useState("123456789")  
+   * const loginHandler = async (event) => {
+      try {
+        event.preventDefault()
+        const user = { email, password }
+        const { data } = await login(user);
+        dispatch(addUser({ user: data?.user, token: data?.token }))
+        console.log(data)
+        if (data?.success) {
+          navigate("/")
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+   */
--- 14.React/rtk-query-auth/src/pages/Register.jsx
+++ 14.React/rtk-query-auth/src/pages/Register.jsx
@@ -1,48 +1,62 @@
 import React, { useState } from 'react'
-import { TextInput } from '@mantine/core';
+import { Loader, TextInput } from '@mantine/core';
 import { PasswordInput } from '@mantine/core';
 import { Link, useNavigate } from 'react-router-dom';
 import { useRegisterMutation } from '../redux/api/authApi';
+import { useForm } from '@mantine/form';
 
 const Register = () => {
+  // Using Mantine use-form
+  const form = useForm({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+    },
 
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [password_confirmation, setPasswordConfirmation] = useState("")
+    validate: {
+      name: (value) => value.length < 2 ? "Name must have at least 2 letters" : null,
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      password: (value) => value.length < 8 ? "Password must have at least 8 letters" : null,
+    },
+  });
 
   const navigate = useNavigate()
-
-  const [register] = useRegisterMutation()
-
-  // Submit event handler
-  const registerHandler = async (event) => {
-    try {
-      event.preventDefault()
-      const user = { name, email, password, password_confirmation }
-      const {data} = await register(user) // returns Promise object
-      //console.log(data.data)
-
-      if (data?.success === true) {
-        navigate('/login')
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const [register, { isLoading, isFetching }] = useRegisterMutation()
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <form onSubmit={registerHandler} className='flex flex-col w-96 gap-10 shadow-lg p-7'>
+      <form onSubmit={form.onSubmit(async (values) => {
+        try {
+          const { data } = await register(values)
+          console.log(data)
+          console.log(values)
+          if (data?.success === true) {
+            navigate('/login')
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      })}
+        className='flex flex-col w-96 gap-10 shadow-lg p-7'>
         <h2 className='text-zinc-900 text-2xl font-semibold'>Register</h2>
 
-        <TextInput value={name} onChange={e => setName(e.target.value)} placeholder='Enter your name' />
+        <TextInput name='name'
+          placeholder='Enter your name'
+          {...form.getInputProps('name')} />
 
-        <TextInput value={email} onChange={e => setEmail(e.target.value)} placeholder='Enter your email' />
+        <TextInput name='email'
+          placeholder='Enter your email'
+          {...form.getInputProps('email')} />
 
-        <PasswordInput value={password} onChange={e => setPassword(e.target.value)} placeholder='Enter your password' />
+        <PasswordInput name='password'
+          placeholder='Enter your password'
+          {...form.getInputProps('password')} />
 
-        <PasswordInput value={password_confirmation} onChange={e => setPasswordConfirmation(e.target.value)} placeholder='Confirm your password' />
+        <PasswordInput name='password_confirmation'
+          placeholder='Confirm your password'
+          {...form.getInputProps('password_confirmation')} />
 
         <div className="flex gap-7">
           <p className='select-none text-zinc-800'>Already have an account?</p>
@@ -51,10 +65,38 @@ const Register = () => {
           </Link>
         </div>
 
-        <button type="submit" className='px-5 py-2 bg-zinc-900 text-white rounded'>Sign Up</button>
+        <button
+          disabled={isLoading && true}
+          type="submit"
+          className='px-5 py-2 bg-zinc-900 text-white rounded'>
+          {isLoading ? (<Loader className='mx-auto block' />) : "Sign In"}
+        </button>
       </form>
     </div>
   )
 }
 
 export default Register
+
+ // Before using mantine use-form
+
+  // const [name, setName] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [password, setPassword] = useState("")
+  // const [password_confirmation, setPasswordConfirmation] = useState("")
+
+  // Submit event handler
+  // const registerHandler = async (event) => {
+  //   try {
+  //     event.preventDefault()
+  //     const user = { name, email, password, password_confirmation }
+  //     const {data} = await register(user) // returns Promise object
+  //     //console.log(data.data)
+
+  //     if (data?.success === true) {
+  //       navigate('/login')
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
